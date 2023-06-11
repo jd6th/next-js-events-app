e898c171f8bbec94ae640f5e0f93b227be320ac7
Joy Dalud <joy.dalud@gmail.com>
2023-06-11 10:48:41 +0800

    add notifications using useContext

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,13 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import Notification from "../events/ui/notification";
 import MainHeader from "./main-header";
 
+import NotificationContext from "../../store/notification-context";
+
 function Layout(props) {
+    const notificationCtx = useContext(NotificationContext);
+    const activeNotification = notificationCtx.notification;
+
+    console.log("activeNotification:::", notificationCtx)
+
     return (
         <Fragment>
             <MainHeader />
             <main>
                 {props.children}
             </main>
+            { activeNotification && (
+                <Notification title={activeNotification.title} message={activeNotification.message} status={activeNotification.status} />
+            )}
         </Fragment>
     )
 }
